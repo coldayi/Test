84eca63b12f3548cc65b97ddc951e0c222eed658
coldayi <151107501+coldayi@users.noreply.github.com>
2024-11-20 18:27:19 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("Hello World")
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+arr = [3, 6, 8, 10, 1, 2, 1]
+print("排序前:", arr)
+print("排序后:", quicksort(arr))
